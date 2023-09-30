c657e163b19e384de96ad7d2d6d95cfa33b3df77
Dow <dhdschool@outlook.com>
2023-09-30 13:14:12 -0500

    adding files

--- app/src/main/java/com/example/entree/MainActivity.kt
+++ app/src/main/java/com/example/entree/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.entree
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.ComponentActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : ComponentActivity(){
 
--- app/src/main/java/com/example/entree/PageActivity.kt
+++ app/src/main/java/com/example/entree/PageActivity.kt
@@ -1,12 +1,49 @@
 package com.example.entree
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.ComponentActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 
 
-class PageActivity: ComponentActivity(){
+class PageActivity: Fragment(){
+//    override fun onCreate(savedInstanceState: Bundle?){
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_page)
+//    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.activity_scroller, container, false)
+
+}
+private const val NUM_PAGES = 3
+class ScreenSlidePagerActivity : FragmentActivity(){
+    private lateinit var viewPager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_page)
+        setContentView(R.layout.activity_scroller)
+
+        viewPager = findViewById(R.id.pager)
+
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        viewPager.adapter = pagerAdapter
+    }
+
+    override fun onBackPressed(){
+        if(viewPager.currentItem ==0){
+            super.onBackPressed()
+        }
+        else{
+            viewPager.currentItem = viewPager.currentItem - 1
+        }
+    }
+
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa){
+        override fun getItemCount() : Int = NUM_PAGES
+        override fun createFragment(position: Int) : Fragment = PageActivity()
     }
 }
