b52a2e024e95341856944c76bb48c5912a9f4a08
Dow <dhdschool@outlook.com>
2023-10-01 09:39:34 -0500

    Fixing all of carsons stuff and JSON object

--- app/src/main/java/com/example/entree/NutritionActivity.kt
+++ app/src/main/java/com/example/entree/NutritionActivity.kt
@@ -9,8 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.fragment.app.Fragment
+import org.json.JSONArray
+import org.json.JSONObject
 
 class NutritionActivity : ComponentActivity() {
 
@@ -18,29 +21,46 @@ class NutritionActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nutrition)
 
-        findViewById<Button>(R.id.matchReturn)
+        findViewById<ImageButton>(R.id.matchReturn)
             .setOnClickListener {
                 val i = Intent(this, PageActivity::class.java)
                 startActivity(i)
             }
+        val json: JSONArray = getJSON()
+
+        if(json != null){
+        val temp = json.getJSONObject(0)
+        System.out.println(temp)
+        if(temp != null){
+        initDms(temp, R.drawable.applejuice)}}
     }
 
-    fun initDms (foodName: String, imgSrc: String, foodData: String){
-        val nameTextView = findViewById<View>(R.id.messageView0)
+    fun getJSON() : JSONArray {
+        val jsonObject = (::KotlinJsonObject)()
+        jsonObject.returnJson()
+        return jsonObject.obj
+    }
+
+    fun initDms(foodName: JSONObject, imgSrc: Int) {
+        val nameTextView = findViewById<TextView>(R.id.messageView0)
+        val calorieTextView = findViewById<TextView>(R.id.messageView1)
+
+        nameTextView.text = foodName.getString("name")
+        calorieTextView.text = foodName.getDouble("calories").toString()
 
     }
 
-    fun editMessage(foodDatum: String){
+//    fun editMessage(foodDatum: String){
+//
+//        for (i in 1..11){
+//            val msgId = "messageView$i"
+//            val msgTextView = findViewById<View>(R.id.msgId)
+//        }
+    // Dear Dow: this obviously doesn't work. There are ways to do it, but Its too late and I want to sleep.
+    // The whole constructing new messages thing didn't work. It's not worth the effort to find a solution, especially considering it wouldn't make a functional difference.
+    // I did seem to find a way to get json objects and strings but im gonna be honest, I didn't really test it or anything. I wanted to get a little bit of everything done.
+    // I'm writing the note because Im either going to be very late or not show up at all. It's 3:30 am. Brain Mush.
+    // Note: they seem to really not want you to change xml text after runtime for some reason. Like, everything in strings.xml for example is permanent. Figure that out FIRST!
 
-        for (i in 1..11){
-            val msgId = "messageView$i"
-            val msgTextView = findViewById<View>(R.id.msgId)
-        }
-        // Dear Dow: this obviously doesn't work. There are ways to do it, but Its too late and I want to sleep.
-        // The whole constructing new messages thing didn't work. It's not worth the effort to find a solution, especially considering it wouldn't make a functional difference.
-        // I did seem to find a way to get json objects and strings but im gonna be honest, I didn't really test it or anything. I wanted to get a little bit of everything done.
-        // I'm writing the note because Im either going to be very late or not show up at all. It's 3:30 am. Brain Mush.
-        // Note: they seem to really not want you to change xml text after runtime for some reason. Like, everything in strings.xml for example is permanent. Figure that out FIRST!
-
-    }
+//}
 }
